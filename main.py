931f50ba948d18c213740381a583816d79a85bfa
Joao Bizzo <brandtjoao7@gmail.com>
2023-09-12 17:59:15 -0300

    update

--- main.py
+++ main.py
@@ -24,16 +24,35 @@ while True:
 
     if switch[opcao] == 'anotar':
         
-        # data = str / KMTotal = float / KMrodado = float /litros=float/ valorPLitro= float/ localização = str
-        
-        print("Formato: ddmmYYYY_aaaaa.aa_xxx.xx_yy.yy_z.zz_'br-araucarias' (data_KMTotal_KMrodado_litros_valorPLitro_localização)")
-
-        nova_info = input('Digite a info de abastecimento: ')
-        
-        with open(path, 'a') as arquivo:
-            arquivo.write(nova_info+'\n')
-        print('Info adicionada com sucesso!')
+        print(" 1 - Anotação fácil ")
+        print(" 2 - Anotação completa ")
+        opcao = int(input('Digite a opção: '))
         print('')
+        if opcao == 1:
+            data = input('Digite a data: ')
+            KMTotal = input('Digite o KM total: ')
+            KMrodado = input('Digite o KM rodado: ')
+            litros = input('Digite a quantidade de litros: ')
+            valorPLitro = input('Digite o valor por litro: ')
+            localizacao = input('Digite a localização: ')
+            print('')
+            nova_info = data+'_'+KMTotal+'_'+KMrodado+'_'+litros+'_'+valorPLitro+'_'+localizacao
+            with open(path, 'a') as arquivo:
+                arquivo.write(nova_info+'\n')
+            print('Info adicionada com sucesso!')
+            print('')
+
+        if opcao == 2:
+            # data = str / KMTotal = float / KMrodado = float /litros=float/ valorPLitro= float/ localização = str
+            
+            print("Formato: ddmmYYYY_aaaaa.aa_xxx.xx_yy.yy_z.zz_'br-araucarias' (data_KMTotal_KMrodado_litros_valorPLitro_localização)")
+
+            nova_info = input('Digite a info de abastecimento: ')
+            
+            with open(path, 'a') as arquivo:
+                arquivo.write(nova_info+'\n')
+            print('Info adicionada com sucesso!')
+            print('')
 
     if switch[opcao] == 'ler':
         with open(path, 'r') as arquivo:
