0aeb553f5e1284327a3a18102e48b69652def97e
Joao Bizzo <brandtjoao7@gmail.com>
2023-10-24 21:31:35 -0300

    add serial

---
+++ bus_analyzer.py
@@ -0,0 +1,15 @@
+import serial
+
+serial_data = 0
+
+vel = serial_data[0]
+rpm = serial_data[1]
+
+if rpm > 700:
+    #carro ligado
+    print('Carro ligado')
+    print('Velocidade: ', vel)
+    print('RPM: ', rpm)
+    print('')
+    
+
